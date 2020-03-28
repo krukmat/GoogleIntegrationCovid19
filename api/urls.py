1f097850720ea321f07e3843e61fac8536129778
Matias Kruk <kruk.matias@gmail.com>
2020-03-28 21:30:04 +0100

    urls error

--- api/urls.py
+++ api/urls.py
@@ -46,12 +46,8 @@ router.register(r'users', UserViewSet)
 router.register(r'organizations', OrganizationViewSet)
 router.register(r'teams', TeamViewSet)
 
-urlpatterns = patterns('',
+urlpatterns = [
 	url(r'^', include(router.urls)),
 	url(r'ping/$', PingView.as_view(), name='ping'),
-	url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
-	url(r'^teams/(?P<team>[^/.]+)/kpis/$', KpiList.as_view(), name='team-kpis'),
-	url(r'^teams/(?P<team>[^/.]+)/kpis/(?P<pk>[^/.]+)/$', KpiDetail.as_view(), name='kpi-detail'),
-	url(r'^teams/(?P<team>[^/.]+)/kpis/(?P<kpi>[^/.]+)/values/$', KpiValueList.as_view(), name='kpi-value-list'),
-	url(r'^teams/(?P<team>[^/.]+)/kpis/(?P<kpi>[^/.]+)/values/(?P<pk>[^/.]+)/$', KpiValueDetail.as_view(), name='kpi-value-detail'),
-)
+	url(r'^auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
