5140190a173910e4d61915bc62ab0366d754ba9b
Matias Kruk <kruk.matias@gmail.com>
2020-03-29 13:02:29 +0200

    url

--- api/views.py
+++ api/views.py
@@ -91,7 +91,8 @@ class ScrapeView(APIView):
 			#word cloud
 			wordcloud = WordCloud().process_text(output)
 			wordcloud = {k: v for k, v in sorted(wordcloud.items(), key=lambda item: item[1], reverse=True)}
-			returnlist.append(dict(cloud=wordcloud, url=url))
+			if wordcloud:
+				returnlist.append(dict(cloud=wordcloud, url=url))
 		return JSONResponseMixin(returnlist)
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
