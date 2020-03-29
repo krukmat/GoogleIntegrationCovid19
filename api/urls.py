73ef03785160c3442352b25a70ba6ddea39664ef
Matias Kruk <kruk.matias@gmail.com>
2020-03-29 14:13:40 +0200

    cleaning up

--- api/urls.py
+++ api/urls.py
@@ -1,50 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from django.conf.urls import url, include
 from api.views import (
-	PingView, ScrapeView, UserViewSet, OrganizationViewSet, TeamViewSet,
-	KpiList, KpiDetail, KpiValueList, KpiValueDetail,
+	PingView, ScrapeView
 )
 
 # This two if you want to enable the Django Admin: (recommended)
 from django.contrib import admin
 admin.autodiscover()
-
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
-
-organization_list = OrganizationViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-
-organization_detail = OrganizationViewSet.as_view({
-	'get': 'retrieve',
-	'put': 'update',
-	'patch': 'partial_update',
-	'delete': 'destroy'
-})
-
-team_list = TeamViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-
-team_detail = TeamViewSet.as_view({
-	'get': 'retrieve',
-	'put': 'update',
-	'patch': 'partial_update',
-	'delete': 'destroy'
-})
-
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'organizations', OrganizationViewSet)
-router.register(r'teams', TeamViewSet)
 
 urlpatterns = [
 	url(r'^', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -21,14 +21,6 @@ from nltk.corpus import stopwords
 import nltk
 from textblob import TextBlob
 
-@api_view(('GET',))
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'organizations': reverse('organization-list', request=request, format=format),
-		'teams': reverse('team-list', request=request, format=format),
-    })
-
 class PingView(APIView):
 
 	def get(self, request):
@@ -98,39 +90,3 @@ class ScrapeView(APIView):
 			if wordcloud:
 				returnlist.append(dict(cloud=wordcloud, url=url))
 		return JSONResponseMixin(returnlist)
-
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
-
-class OrganizationViewSet(viewsets.ModelViewSet):
-	queryset = Organization.objects.all()
-	serializer_class = OrganizationSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-	def perform_create(self, serializer):
-		serializer.save(owner=self.request.user)		
-
-class TeamViewSet(viewsets.ModelViewSet):
-	queryset = Team.objects.all()
-	serializer_class = TeamSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)	
-
-
-#TODO: The views below could be transformed into viewsets later
-
-class KpiList(generics.ListCreateAPIView):
-	queryset = Kpi.objects.all()
-	serializer_class = KpiSerializer
-
-class KpiDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Kpi.objects.all()
-	serializer_class = KpiSerializer
-
-class KpiValueList(generics.ListCreateAPIView):
-	queryset = KpiValue.objects.all()
-	serializer_class = KpiValueSerializer
-
-class KpiValueDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = KpiValue.objects.all()
-	serializer_class = KpiValueSerializer
